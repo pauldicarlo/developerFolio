a37fbb217d29791b04deca17907c0939f1127e4a
Paul DiCarlo <pdicarlo@us.ibm.com>
2025-11-24 11:42:36 -0600

    initial update for testing

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,10 +20,10 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Paul DiCarlo",
+  title: "Hello.  I'm Paul DiCarlo",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A software enginner having experience of building solutions using IBM Cloud / Golang / Java / Python."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
@@ -33,15 +33,9 @@ const greeting = {
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
-  // Instagram, Twitter and Kaggle are also supported in the links!
-  // To customize icons and social links, tweak src/components/SocialMedia
+  github: "https://github.com/pauldicarlo",
+  linkedin: "https://www.linkedin.com/in/pauldicarlo/",
+  gmail: "paul.dicarlo@gmail.com",
   display: true // Set true to display this section, defaults to false
 };
 
